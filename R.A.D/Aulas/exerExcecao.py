8102d0025575e7814c2ab35be975de9dde323b1f
Luis Eduardo <72215228+luisiasd@users.noreply.github.com>
2021-04-03 18:43:13 -0300

    Delete R.A.D/Aulas directory

--- R.A.D/Aulas/exerExcecao.py
+++
@@ -1,9 +0,0 @@
-#-------------------Zero - Division - Error ---------------
-def main ():
-    try:
-        x = 10 * (1/0)
-    except ZeroDivisionError:
-        print("Divisão por Zero não permitida!")
-        
-#----------------------------------------------------------
-main()
